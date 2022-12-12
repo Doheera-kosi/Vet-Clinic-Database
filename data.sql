a3abdab781db507a245e0732010e803047f3f01a
Doheera-kosi <kupourevans99@gmail.com>
2022-12-12 15:16:47 +0000

    Update date format

--- data.sql
+++ data.sql
@@ -2,16 +2,16 @@
 
 INSERT INTO public.animals(
 	id, name, date_of_birth, escape_attempts, neutered, weight_kg)
-	VALUES (0001, 'Agumon', '03/02/2020', 0, true, 10.23);
+	VALUES (0001, 'Agumon', '2020/02/03', 0, true, 10.23);
 
 INSERT INTO public.animals(
 	id, name, date_of_birth, escape_attempts, neutered, weight_kg)
-	VALUES (0002, 'Gabumon', '11/15/2018', 2, true, 8);
+	VALUES (0002, 'Gabumon', '2018//11/15', 2, true, 8);
 	
 INSERT INTO public.animals(
 	id, name, date_of_birth, escape_attempts, neutered, weight_kg)
-	VALUES (0003, 'Pikachu', '07/01/2021', 1, false, 15.04);
+	VALUES (0003, 'Pikachu', '2021/01/07', 1, false, 15.04);
 	
 INSERT INTO public.animals(
 	id, name, date_of_birth, escape_attempts, neutered, weight_kg)
-	VALUES (0004, 'Devimon', '12/05/2017', 5, true, 11);
+	VALUES (0004, 'Devimon', '2017/05/12/', 5, true, 11);
