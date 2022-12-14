c20f64719234ab3a915b112fed9c131d9c125945
Doheera-kosi <kupourevans99@gmail.com>
2022-12-14 14:19:44 +0000

    Query multiple tables activity

--- data.sql
+++ data.sql
@@ -45,3 +45,85 @@ VALUES (0010, 'Blossom', '1998/13/10', 3, true, 17);
 INSERT INTO public.animals (
 	id, name, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES (0011, 'Ditto', '2022/14/05', 4, true, 22);
+
+
+-- Inserting Data into owners table
+
+INSERT INTO public.owners (full_name, age)
+	VALUES ('Sam Smith', 34);
+
+INSERT INTO public.owners (full_name, age)
+	VALUES ('Jennifer Orwell', 19);
+
+INSERT INTO public.owners (full_name, age)
+	VALUES ('Bob', 45);
+
+INSERT INTO public.owners (full_name, age)
+	VALUES ('Melody Pond', 77);
+
+INSERT INTO public.owners (full_name, age)
+	VALUES ('Dean winchester', 14);
+
+INSERT INTO public.owners (full_name, age)
+	VALUES ('Jodie Whittaker', 38);
+
+
+
+-- Inserting into species
+
+INSERT INTO public.species (name)
+	VALUES ('Pokemon');
+
+INSERT INTO public.species (name)
+	VALUES ('Digimon');
+
+
+/* Update species_id col in animals table with a condition */
+-- Begin Transaction
+BEGIN;
+
+-- update species col for animals with names ending in mon
+UPDATE animals
+SET species_id = 2
+WHERE name LIKE '%mon';
+
+-- update species col for animals with names not ending in mon
+UPDATE animals
+SET species_id = 1
+WHERE name NOT LIKE '%mon';
+
+-- commit transaction
+COMMIT;
+
+
+-- Modifying inserted animals to include owners information
+-- BEGIN TRANSACTIONS
+BEGIN;
+
+--- Sam Smith owns Agumon.
+UPDATE animals
+SET owners_id = 1
+WHERE name = 'Agumon';
+
+-- Jennifer Orwell owns Gabumon and Pikachu.
+UPDATE animals
+SET owners_id = 2
+WHERE name IN ('Gabumon', 'Pikachu');
+
+-- Bob owns Devimon and Plantmon.
+UPDATE animals
+SET owners_id = 3
+WHERE name IN ('Devimon', 'Plantmon');
+
+-- Melody Pond owns Charmander, Squirtle, and Blossom.
+UPDATE animals
+SET owners_id = 4
+WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
+
+-- Dean Winchester owns Angemon and Boarmon.
+UPDATE animals
+SET owners_id = 5
+WHERE name IN ('Angemon', 'Boarmon');
+
+--- commit transaction
+COMMIT;
--- schema.sql
+++ schema.sql
@@ -17,3 +17,53 @@ ALTER TABLE IF EXISTS public.animals
 // Adding a COLUMN species
 ALTER TABLE IF EXISTS public.animals
     ADD COLUMN species character varying;
+
+
+-- Query Multiple tables
+
+-- Create table owners
+
+CREATE TABLE public.owners
+(
+    id bigserial NOT NULL,
+    full_name character varying,
+    age integer,
+    PRIMARY KEY (id)
+);
+
+ALTER TABLE IF EXISTS public.owners
+    OWNER to postgres;
+
+-- Create table species
+CREATE TABLE public.species
+(
+    id bigserial NOT NULL,
+    name character varying,
+    PRIMARY KEY (id)
+);
+
+ALTER TABLE IF EXISTS public.species
+    OWNER to postgres;
+
+
+-- Modifying Animals table
+ALTER TABLE animals
+    ADD COLUMN id BIGSERIAL PRIMARY KEY
+
+-- Removing species from animals table
+ALTER TABLE animals
+	DROP COLUMN species
+
+ALTER TABLE animals
+	ADD COLUMN species_id int,
+
+ALTER TABLE animals
+	ADD COLUMN owners_id int,
+
+ALTER TABLE animals
+	ADD CONSTRAINT fk_species FOREIGN KEY(species_id) REFERENCES species(id),
+	
+ALTER TABLE animals
+	ADD CONSTRAINT fk_owners FOREIGN KEY(owners_id) REFERENCES owners(id);
+
+
