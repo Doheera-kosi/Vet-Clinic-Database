84538986761843c02ce8318d79d0a7843cfba785
Dave ZAGABE <80624123+DaveZag@users.noreply.github.com>
2022-12-20 15:46:19 +0200

    Add 2 new tables and database indexes

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -37,3 +37,31 @@ CREATE TABLE treatments (
 );
 
 --- create invoice_items table
+CREATE TABLE invoice_items (
+  id SERIAL NOT NULL,
+  unit_price DECIMAL(10,2) NOT NULL,
+  quantity INT NOT NULL,
+  total_amount DECIMAL(10,2) NOT NULL,
+  invoice_id INT,
+  treatment_id INT,
+  PRIMARY KEY (id),
+  FOREIGN KEY (invoice_id) REFERENCES invoices(id),
+  FOREIGN KEY (treatment_id) REFERENCES treatments(id),
+);
+-- Creating treatment_histories join table
+-- creating invoice_items table
+CREATE TABLE treatment_histories (
+  id SERIAL NOT NULL,
+  medical_history_id INT,
+  treatment_id INT,
+  PRIMARY KEY (id),
+  FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id),
+  FOREIGN KEY (treatment_id) REFERENCES treatments(id),
+);
+-- CREATE FK INDEXES
+CREATE INDEX ON medical_histories(patient_id);
+CREATE INDEX ON invoices(medical_history_id);
+CREATE INDEX ON invoice_items(invoice_id);
+CREATE INDEX ON invoice_items(treatment_id);
+CREATE INDEX ON treatment_histories(medical_history_id);
+CREATE INDEX ON treatment_histories(treatment_id);
